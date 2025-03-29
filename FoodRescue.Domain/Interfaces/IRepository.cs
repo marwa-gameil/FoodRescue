44312ca66a0f9ec9a4c16367efafcd2f55a06180
marwa-gameil <marwagamil666@gmail.com>
2025-03-29 23:01:10 +0200

    [ADD] save methods to generic repo

--- FoodRescue.Domain/Interfaces/IRepository.cs
+++ FoodRescue.Domain/Interfaces/IRepository.cs
@@ -1,6 +1,7 @@
 using System.Linq.Expressions;
 using FoodRescue.Domain.Abstractions;
 using FoodRescue.Domain.Specifications;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodRescue.Domain.Interfaces;
 
@@ -11,7 +12,12 @@ public interface IRepository<TModel> where TModel : BaseModel
     Task<IEnumerable<TResult>> GetAll<TResult>(Specification<TModel, TResult> specification);
     Task<TModel?> GetOne(Specification<TModel> specification);
     Task<TResult?> GetOne<TResult>(Specification<TModel, TResult> specification);
+    Task<TModel?> GetById(Guid id);
     Task<TModel> Add(TModel model);
     TModel Update(TModel model);
+
     void Delete(TModel model);
+    Task Save();
+
 }
+
